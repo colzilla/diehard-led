8de82d461fb41c524d9310d7e882633c14c011f7
Colin Taylor <5434186+colzilla@users.noreply.github.com>
2020-01-17 06:55:56 -0800

    cleaned up

--- trinket-M0/code.py
+++ trinket-M0/code.py
@@ -5,36 +5,25 @@ import adafruit_dotstar as dotstar
 import touchio
 import neopixel
 
-# On-board DotStar for boards including Gemma, Trinket, and ItsyBitsy
 dots = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=1)
 pixels = neopixel.NeoPixel(board.A3, 100, brightness=1, auto_write=True)
 
 touch_pad = board.A0
 
-# HELPERS
-# a random color 0 -> 224
-def random_color():
-    return random.randrange(0, 7) * 32
-
 touch = touchio.TouchIn(touch_pad)
+
 n = 0.3
 i = 0
 m = 10000
+speed=0.02
+
 dots[0] = (0x000000)
 pixels.fill(0x000000)
 
-# while True:
-#     print("pulses[{}])".format(pulses[0]))
-#     speed=0.200
-#     time.sleep(speed)
 
 while True:    
     print(touch.value)
-    # print("pulses[{}])".format(pulses[0]))
-    # pixels[5] = 0x0000ff
-    # time.sleep(0.4)
 
-    speed=0.02
     colour=0xff0000
     for led in range(0, 5):
         print("led[{}]".format(led))
@@ -183,29 +172,3 @@ while True:
         while i < m and not touch.value : i=i+1
         while not touch.value and i < m: pass       
         time.sleep(n)
-
-        while True:
-            # print("pulses[{}])".format(pulses[0]))
-            speed=0.002
-            # time.sleep(speed)
-
-            colour=0xff0000
-            for led in range(0, 24):
-                # print("led[{}]".format(led))
-                pixels[led + 5] = colour
-                pixels[led + 5 + 24] = colour
-                time.sleep(speed)
-
-            colour=0x888888
-            for led in range(0, 24):
-                # print("led[{}]".format(led))
-                pixels[led + 5] = colour
-                pixels[led + 5 + 24] = colour
-                time.sleep(speed)
-
-            colour=0x0000ff
-            for led in range(0, 24):
-                # print("led[{}]".format(led))
-                pixels[led + 5] = colour
-                pixels[led + 5 + 24] = colour
-                time.sleep(speed)
